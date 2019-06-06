3a44de5b3765be924dffd82ca2f3d6596edf3e3c
Vernieri <jvmendes.ti@gmail.com>
2019-06-06 09:52:52 -0300

    Update apoteose.py

--- Apoteose/apoteose.py
+++ Apoteose/apoteose.py
@@ -5,3 +5,9 @@ import hashlib
 
 def call():
 	process()
+
+def shaFunc(string):
+    """
+    Return a SHA-256 hash of the given string
+    """
+    return hashlib.sha256(string.encode('utf-8')).hexdigest()	
