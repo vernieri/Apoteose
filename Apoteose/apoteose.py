74c5a7d331c3d1fc80c44e01f049681cef65a0cf
Vernieri <jvmendes.ti@gmail.com>
2019-06-07 17:12:49 -0300

    Update apoteose.py

--- Apoteose/apoteose.py
+++ Apoteose/apoteose.py
@@ -78,9 +78,12 @@ def shaFunc(string):
     Return a SHA-256 hash of the given string
     """
     return hashlib.sha256(string.encode('utf-8')).hexdigest()	
+
 def intoNum(stringList):
 	sha1 = shaFunc(stringList[0])
 	sha2 = shaFunc(stringList[1])
+	#print(sha1)
+	#print(sha2)
 
 	n15 = 0
 	n25 = 0
@@ -92,44 +95,34 @@ def intoNum(stringList):
 	str5 = 'e'
 	str6 = 'f'
 
-	#print("Antes:")
-	#print(shaFunc(s1))
-	#print(shaFunc(s2))
 	if sha1.find(str1 or str2 or str3 or str4 or str5 or str6):
 		n10 = sha1.replace('a', '4')
-		n11 = n10.replace('b', '3')
-		n12 = n11.replace('c', '2')
-		n13 = n12.replace('d', '5')
+		n11 = n10.replace('b', '5')
+		n12 = n11.replace('c', '1')
+		n13 = n12.replace('d', '3')
 		n14 = n13.replace('e', '6')
-		n15 = n14.replace('f', '7')
+		n15 = n14.replace('f', '8')
 
 	else:
 		print("[-] ERROR 0x01A")
+		exit()
 
-	#print("depois:")
-	#print(n15)
-	#print(int(n15))
 
 	if sha2.find(str1 or str2 or str3 or str4 or str5 or str6):
-		n20 = sha1.replace('a', '4')
-		n21 = n20.replace('b', '3')
-		n22 = n21.replace('c', '2')
-		n23 = n22.replace('d', '5')
-		n24 = n23.replace('e', '6')
-		n25 = n24.replace('f', '7')
+		n20 = sha1.replace('a', '2')
+		n21 = n20.replace('b', '4')
+		n22 = n21.replace('c', '7')
+		n23 = n22.replace('d', '9')
+		n24 = n23.replace('e', '0')
+		n25 = n24.replace('f', '6')
 
 	else:
 		print("[-] ERRO 0x01B")
+		exit()
 
-	#print("depois:")
-	#print(n15)
 	bigNum = (int(n15)*int(n25))	
-	#bigNum = int(n15)*int(n25)
-	#bigString = str(bigNum).replace('2', '0\n')
-	#print(bigString)
 
 	return bigNum
-
 def intoList(bigNum, qt, num):
 	if num != 0:
 		print('Todo')
