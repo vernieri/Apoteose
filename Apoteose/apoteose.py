d1e36bac2f4ce9a991a54a26cc0746e8211ee277
Vernieri <jvmendes.ti@gmail.com>
2020-05-10 23:51:50 -0300

    Update apoteose.py

--- Apoteose/apoteose.py
+++ Apoteose/apoteose.py
@@ -1,4 +1,15 @@
+import time
+import re
+import hashlib
 
+def shaFunc(v1, v2):
+
+    x = int(v1[9:12])**int(v2[9:12])
+    y = int(v2[8:11])**int(v1[8:11])
+    #print(x)
+
+    string = v1+v2+str(x)+str(y)
+    return hashlib.sha256(string.encode('utf-8')).hexdigest()
 
 
 def procedure():
