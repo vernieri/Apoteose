28cedffbc5f9a69f5154d1f2e153e853493d8459
Vernieri <jvmendes.ti@gmail.com>
2020-05-11 00:01:40 -0300

    Update apoteose.py
    Apoteose() func

--- Apoteose/apoteose.py
+++ Apoteose/apoteose.py
@@ -11,6 +11,19 @@ def shaFunc(v1, v2):
     string = v1+v2+str(x)+str(y)
     return hashlib.sha256(string.encode('utf-8')).hexdigest()
 
+def apoteose():
+	values = process()
+	hash1 = shaFunc(values[0], values[1])
+	hash2 = shaFunc(values[1], values[0])
+
+	#print(hash1, hash2)
+
+	n1 = re.findall(r'\d+', str(hash1))
+	n2 = re.findall(r'\d+', str(hash2))
+
+	number = int(n1[0])+int(n2[1])
+	return number
+
 
 def procedure():
    time.sleep(0.0005)
