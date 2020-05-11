ecd3f3b5fe48f4a5401a71362056f1288f964509
Vernieri <jvmendes.ti@gmail.com>
2020-05-10 23:48:58 -0300

    Update getRandom.py

--- Apoteose/getRandom.py
+++ Apoteose/getRandom.py
@@ -1,19 +1,13 @@
 #!/usr/bin/python3
 from Apoteose import *
 
-def getValue():
-	#simpleRandomList() #Return a simple random numbers list #Done
-	b = randomList(100) #return a specific qt of numbers #Done
-	#randomWord('dicionario', 100) #return a random word from a List or File
-	#keyRandom(key, qt) #You Pass a Key, that key will be used to make randomic values, then return
-	#randomOdds(qt) #Qt of odds numbers you want
-	#randomPairs(qt) #qt of pairs numbers
-	#randomToken() #generate a Token
-	#randomHugeNumbers(num, qt) #you choose the size of numbers. 2 digits? 6 digits?
-	#randomInsane(rep, qt) #You can loop the core to generate more Randomic values.
+x = apoteose()
+print(x)
 
-	#print('%s' % x)
-	print(b)
-getValue()	
+x = apoteoseList()
+print(x)
+
+x = apoteoseRange()
+print(x)
 
 
